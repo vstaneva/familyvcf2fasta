2601fcaa6b4c3b457e09a5ff9d44048f6ee3b5ac
Valeria Staneva <vstaneva@mit.edu>
2015-08-04 23:46:04 +0300

    Documented the main script

--- mfcVCFtoFASTA.py
+++ mfcVCFtoFASTA.py
@@ -1,8 +1,12 @@
 import ConfigParser
 import sys
-import time
 
 def prepMember(member, configpath):
+	"""
+	Assuming configpath is a config file and member is either Child,
+	Mother of Father. f1 and f2 are the paths to the files where the
+	two sequences for this member are going to be scored.
+	"""
 	config = ConfigParser.RawConfigParser()
 	config.read(configpath)
 	ref = config.get("Common", "reference")
@@ -13,17 +17,29 @@ def prepMember(member, configpath):
 	return [ref, win, vcf, f1, f2]
 
 def VCFtoFASTA(member):
+	"""
+	Assuming that member gives us the information about a child, a mother
+	or a father of the trio. We first build the reference window from the FASTA
+	file, then apply the variants from the VCF file to form two FASTA sequences
+	-- the first one takes all non-overlapping variants from left to right, and
+	the second one takes the ones that can't be put in the first, as well as the
+	homozygous variants. This process is described in [Makinen and Valenzuela, 2015].
+
+	After building the two sequences, we align them by putting gaps where insertions
+	and deletions have occured. This is not optimal alignment, but it works for us.
+	Then, we write these aligned sequences to the FASTA files.
+	"""
 	ref = open(member[0], "r")
 	window = open(member[1], "r")
 	vcffile = open(member[2], "r")
 	fasta1 = open(member[3], "w")
 	fasta2 = open(member[4], "w")
 	
-	chrom = ">"+window.readline()
-	start = int(window.readline())
-	finish = int(window.readline())
+	chrom = ">"+window.readline() #in the FASTA genome file, every chromosome is a header and starts with ">"
+	start = int(window.readline()) #this is the start of the window (included in the reference window)
+	finish = int(window.readline()) #this is the end of the window (included in the reference window)
 
-	refwin = []
+	refwin = [] #a list that corresponds to the reference window between the start and the end
 
 
 	#first, we get the FASTA sequence between positions start and finish
@@ -35,7 +51,7 @@ def VCFtoFASTA(member):
 	posskipped = 0
 
 	for line in ref:
-		posskipped+=60 #this is how long a FASTA line is
+		posskipped+=60 #this is how long we assume that a FASTA line is
 		if posskipped<start:
 			continue
 		refwin+=list(line.strip().upper())
@@ -44,54 +60,55 @@ def VCFtoFASTA(member):
 	refwin = refwin[(start-1)%60:] #trim chars before the start position
 	refwin = refwin[:-(60-finish%60)] #trim chars after the finish; turns out that if finish%60==0, then refwin has +60 chars
 
-	sequence1 = list(refwin)
-	sequence2 = list(refwin)
-	used1 = [0]*len(refwin)
-	used2 = [0]*len(refwin)
-	ind1 = [0]*len(refwin)
+	sequence1 = list(refwin) 
+	sequence2 = list(refwin) 
+	used1 = [0]*len(refwin) #indicates if an index is used to check for overlapping variants
+	used2 = [0]*len(refwin) #see above
+	ind1 = [0]*len(refwin) #indicates where are insertions/deletions
 	ind2 = [0]*len(refwin)
 
-	homozygous = False
+	homozygous = False #a homozygous variant is applied to both sequences
 
 	#second, we insert the changes from the VCF into the sequences
 	for line in vcffile:
-		if line[0]=='#':
+		if line[0]=='#': #header line
 			continue
-		if not line[0] == chrom:
+		if not line[0] == chrom: #wrong chromosome
 			continue
 		info = line.split()
 		pos = int(info[1])
 		refpos = list(info[3].strip())
 		altpos = list(info[4].strip())
-		if pos<start:
+		if pos<start: #haven't reached the window yet
 			continue
-		if pos>finish:
+		if pos>finish: #went past the window
 			break
-		#we are surely in that range
+		#we are surely in the window
 		#now we check whether the variant is homozygous or heterozygous
 		homozygous = True if info[9][0]=='1' and info[9][2]=='1' else False
 		winpos = pos-start
 		si1 = sum(ind1[:winpos]) #what is the difference in positions between ref and sequence1
 		si2 = sum(ind2[:winpos]) #what is the difference in positions between ref and sequence2
-		uscore1 = sum(used1[winpos+si1:winpos+len(refpos)+si1])
+		uscore1 = sum(used1[winpos+si1:winpos+len(refpos)+si1]) #is there anything used in the positions of the variant
 		uscore2 = sum(used2[winpos+si2:winpos+len(refpos)+si2])
 		if uscore1 == 0: #we can add this variant to the 1st FASTA file
 			sequence1[winpos+si1:winpos+len(refpos)+si1] = list(altpos)
 			ind1[winpos] = len(altpos)-len(refpos)
 			used1[winpos+si1:winpos+len(refpos)+si1] = [1]*len(refpos)
-			if not homozygous:
+			if not homozygous: #we wouldn't want to add to the second sequence
 				continue
 		if uscore2 == 0:
 			sequence2[winpos+si2:winpos+len(refpos)+si2] = list(altpos)
 			ind2[winpos] = len(altpos)-len(refpos)
 			used2[winpos+si2:winpos+len(refpos)+si2] = [1]*len(refpos)
 
-	gappedsequence1 = list(sequence1)
+	gappedsequence1 = list(sequence1) #in these we put the aligned sequences
 	gappedsequence2 = list(sequence2)
 
 	gaps1 = 0
 	gaps2 = 0
-	#now, we build the alignment of the sequences
+	#now, we build the alignment of the sequences (in fact, only semi-align: put gaps where there are INDELS)
+	#put gaps ('-'s) with respect to the first sequence
 	for nucind in xrange(len(ind1)):
 		gaplen = ind1[nucind]*((-1) if ind1[nucind]<0 else 1)
 		if ind1[nucind]>0: #there was an insertion
@@ -104,6 +121,7 @@ def VCFtoFASTA(member):
 			gaps1+=gaplen	
 	gaps1 = 0
 	gaps2 = 0
+	#put gaps with respect to the second sequence
 	for nucind in xrange(len(ind2)):
 		gaplen = ind2[nucind]*((-1) if ind2[nucind]<0 else 1)
 		if ind2[nucind]>0: #there was an insertion
@@ -114,11 +132,7 @@ def VCFtoFASTA(member):
 			#add gaps to sequence2 and make sure ind is OK too
 			gappedsequence2[nucind+gaps2:nucind+gaps2+1] = gappedsequence2[nucind+gaps2:nucind+gaps2+1] + ['-']*gaplen
 			gaps2+=gaplen	
-	#watch out: i have a feeling that if this fails occasionally,
-	#it would be because I only check for sequence1's insertions and deletions
-	#but I don't check for sequence2's insertions and deletions
-
-	#TODO: construct a test where sequence2 also has some indels and see how this behaves. UPDATE: this might be actually working...
+	#watch out: it might turn out that this alignment is not enough
 	
 	#lastly, we format the aligned sequences as FASTA (60 characters per line)
 	fasta1.write(">hg19|chromosome "+chrom[1:-1]+"|start pos "+str(start)+"|end pos "+str(finish)+"|variants from "+member[2]+"|first sequence\n")
