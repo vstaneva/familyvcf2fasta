c35c85ef9758251b0dd190c5ec72b2b9a71e95f8
Valeria Staneva <vstaneva@mit.edu>
2015-08-05 03:24:33 +0300

    Updated documentation of writeconfig.py

--- writeconfig.py
+++ writeconfig.py
@@ -4,11 +4,15 @@ config = ConfigParser.RawConfigParser()
 
 # When adding sections or items, add them in the reverse order of
 # how you want them to be displayed in the actual file.
-# In addition, please note that using RawConfigParser's and the raw
-# mode of ConfigParser's respective set functions, you can assign
-# non-string values to keys internally, but will receive an error
-# when attempting to write to a file or when you get it in non-raw
-# mode. SafeConfigParser does not allow such assignments to take place.
+
+#The common section includes a FASTA reference file and a .posinfo file
+#The .posinfo file has three lines: the chromosome, and the start and end
+#of the region that will be phased. Interesting regions can be obtained
+#by running help_scripts/findInterestingRegions.py
+#The Child, Mother and Father sections each contain a VCF file
+#for that individual, as well as paths to the two aligned FASTA files that
+#contain the non-overlapping variants,
+#as described in [Makinen and Valenzuela, 2015]
 config.add_section('Common')
 config.set('Common', 'reference', 'build37-chr21.fa')
 config.set('Common', 'window', 'NA12880-chr21.posinfo')
@@ -25,7 +29,7 @@ config.set('Father', 'vcf_file', 'NA12877-chr21-clean.vcf')
 config.set('Father', 'fasta1', 'NA12877-chr21_1.fa')
 config.set('Father', 'fasta2', 'NA12877-chr21_2.fa')
 
-# Writing our configuration file to 'example.cfg'
+# Writing our configuration file to 'family.config'
 with open('family.config', 'wb') as configfile:
     config.write(configfile)
 
