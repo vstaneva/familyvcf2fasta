61f6bb6d732de33ab304c79d1148ede9cab36cd0
Valeria Staneva <vstaneva@mit.edu>
2015-08-07 03:16:16 +0300

    Read the phased string.

--- mfcVCFtoFASTA.py
+++ mfcVCFtoFASTA.py
@@ -183,6 +183,10 @@ def callSimilarityPhaser(mother, father, child):
 	subprocess.check_call("make -C phasing_family/src", shell=True)
 	subprocess.check_call("phasing_family/src/mfc_similarity_phaser {0} {1} {2} {3} {4} {5} ".format(mother_fasta1, mother_fasta2, father_fasta1, father_fasta2, child_fasta1, child_fasta2), shell=True)
 	
+def phasedStringToVCF(child):
+	stringfile = open("phasing_family/src/phase_string.txt", "r")
+	phstring = stringfile.read().strip()
+
 def vcfToFasta():	
 	try:	
 		configuration = sys.argv[1]
