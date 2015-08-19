9db9f31171f2acd14cdaf77f14f05bb1d24cc75d
Daniel Valenzuela <daniel.valenzuela.serra@gmail.com>
2015-08-19 16:28:06 +0300

    compiling with full optimization

--- phasing_family/src/mfc_similarity_phaser.cpp
+++ phasing_family/src/mfc_similarity_phaser.cpp
@@ -91,6 +91,9 @@ int main(int argc, char ** argv) {
   }
   
   double time = Utils::StopClock();
+  if (score_1 == score_2) {
+    fprintf(stderr,"WARNING: Different scores after changing the order of params, this should not occur\n");
+  }
   assert(score_1 == score_2);
   
   char * consensus = new char[child_len];
